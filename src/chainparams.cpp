ccb5ddd0c728a3cad9f7e5c554cce87546f4160f
misternode <misterynode@gmail.com>
2018-07-28 18:21:35 +0000

    Change DAA to Dark Gravity sooner (block height 22500)

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -120,7 +120,7 @@ public:
 		consensus.fPowAllowMinDifficultyBlocks = false;
 		consensus.fPowNoRetargeting = false;
 		consensus.nPowKGWHeight = 15200;
-		consensus.nPowDGWHeight = 34140;
+		consensus.nPowDGWHeight = 22500;
 		consensus.nRuleChangeActivationThreshold = 1916; // 95% of 2016
 		consensus.nMinerConfirmationWindow = 2016; // nPowTargetTimespan / nPowTargetSpacing
 		consensus.vDeployments[Consensus::DEPLOYMENT_TESTDUMMY].bit = 28;
