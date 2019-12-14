8d894e8143d988135d5ea1872811afd05c8a3126
Rene Windegger <rene@windegger.wtf>
2019-12-14 16:42:51 +0100

    Just doing a swallow clone.

--- contrib/gitian-build.sh
+++ contrib/gitian-build.sh
@@ -233,9 +233,9 @@ echo ${COMMIT}
 if [[ $setup = true ]]
 then
     sudo apt-get install ruby apache2 git apt-cacher-ng python-vm-builder qemu-kvm qemu-utils
-    git clone https://github.com/mogwaicoin/gitian.sigs.git
-    git clone https://github.com/mogwaicoin/mogwai-detached-sigs.git
-    git clone https://github.com/devrandom/gitian-builder.git
+    git clone --depth 1 https://github.com/mogwaicoin/gitian.sigs.git
+    git clone --depth 1 https://github.com/mogwaicoin/mogwai-detached-sigs.git
+    git clone --depth 1 https://github.com/devrandom/gitian-builder.git
     pushd ./gitian-builder
     if [[ -n "$USE_LXC" ]]
     then
