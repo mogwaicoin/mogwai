de73c3bb63ac00525eff5efef79e7b611ce686ed
Rene Windegger <rene@windegger.wtf>
2019-12-20 09:29:06 +0100

    Revert "Fixed a compile error."
    This reverts commit 458bd1640e52bf623643da068286a12693cdfa6c.

--- src/masternodeconfig.cpp
+++ src/masternodeconfig.cpp
@@ -73,7 +73,7 @@ bool CMasternodeConfig::read(std::string& strErr) {
 //                        strprintf(_("(must be %d for mainnet)"), mainnetDefaultPort);
 //                streamConfig.close();
 //                return false;
-//            }
+            }
         } else if(port == mainnetDefaultPort) {
             strErr = _("Invalid port detected in masternode.conf") + "\n" +
                     strprintf(_("Line: %d"), linenumber) + "\n\"" + line + "\"" + "\n" +
